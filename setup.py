3ab473930194d283b0424a6962fa2ed7ea714ee9
Sean Harrison <sah@kruxia.com>
2023-07-21 15:58:42 -0500

    WIP repository:

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
         install_requires=[
             "base58~=2.1.1",
             "orjson~=3.9.2",
-            "psycopg[binary]~=3.1.9",
+            "psycopg[binary,pool]~=3.1.9",
             "pydantic~=2.0.3",
             "sqly[migration] @ git+https://github.com/kruxia/sqly@00f46ee136e9acc286a2b734413a91dd8a6a8102#egg=sqly",  # noqa
             "python-ulid~=1.1.0",
