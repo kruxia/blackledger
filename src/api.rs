e2e7979f19b6225f657b4dd42002ca5c1619875b
Sean Harrison <sah@kruxia.com>
2023-11-11 15:14:18 -0600

    rustup - /api in nested router

--- src/api.rs
+++ src/api.rs
@@ -1,1 +1,8 @@
+use crate::api::home::home;
+use axum::{routing::get, Router};
+
 pub mod home;
+
+pub fn api_router() -> Router {
+    Router::new().route("/", get(home))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use crate::api::home::home;
-use axum::{routing::get, Router};
+use crate::api::api_router;
+use axum::Router;
 
 pub mod api;
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/api", get(home));
+    let app = Router::new().nest("/api", api_router());
 
     // run it with hyper on localhost:8000
     axum::Server::bind(&"0.0.0.0:8000".parse().unwrap())
