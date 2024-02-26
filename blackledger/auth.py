9025a527e43c8cba81ccb680b9c82f90f5755743
Sean Harrison <sah@kruxia.com>
2024-02-26 17:13:25 -0600

    authn - API requires auth - blackledger.auth.JWTAuthorization usable as a  dependency - add AuthSettings and new Settings objects - make auth_client fixture for auth tests - disable auth in client fixture for non-auth tests

---
+++ blackledger/auth.py
@@ -0,0 +1,53 @@
+import logging
+import re
+import traceback
+from http import HTTPStatus
+
+import jwt
+from fastapi import HTTPException
+from fastapi.security import APIKeyHeader
+from starlette.requests import Request
+
+LOG = logging.getLogger(__name__)
+
+
+class JWTAuthorization(APIKeyHeader):
+    TOKEN_PATTERN = re.compile(r"^.*Bearer\s*(.*)$", flags=re.I)
+
+    def __init__(self, auth_settings, **kwargs):
+        super().__init__(**kwargs)
+        self.settings = auth_settings
+        self.client = jwt.PyJWKClient(self.settings.jwks_url)
+
+    async def __call__(self, request: Request):
+        if request.app.settings.auth.disabled is True:
+            return
+        try:
+            bearer = request.headers.get("Authorization")
+            assert bearer is not None, "Authorization header not found"
+            request.state.auth_claims = self.decode(bearer)
+        except Exception as exc:
+            LOG.info(traceback.format_exc())
+            raise HTTPException(
+                status_code=HTTPStatus.UNAUTHORIZED,
+                detail=f"Unauthorized: {exc.__class__.__name__}: {str(exc)}",
+            )
+
+    def decode(self, bearer):
+        token = re.sub(self.TOKEN_PATTERN, r"\1", bearer)
+        key = self.client.get_signing_key_from_jwt(token).key
+        claims = jwt.decode(
+            token,
+            key,
+            algorithms=[self.settings.alg],
+            issuer=self.settings.iss,
+            options=dict(
+                require=["iss", "iat", "exp", "sub"],
+                verify_signature=True,
+                verify_aud=bool(self.settings.aud),  # only verify aud if configured
+                verify_exp=True,
+                verify_iss=True,
+            ),
+        )
+        LOG.debug(f"{claims=}")
+        return claims
--- blackledger/tests/conftest.py
+++ blackledger/tests/conftest.py
@@ -2,7 +2,6 @@ import pytest
 
 from blackledger.tests import fixtures
 
-client = pytest.fixture(scope="session")(fixtures.client)
 dbpool = pytest.fixture(scope="session")(fixtures.dbpool)
 sql = pytest.fixture(scope="session")(fixtures.sql)
 json_dumps = pytest.fixture(scope="session")(fixtures.json_dumps)
@@ -17,6 +16,8 @@ base_tenant_name = pytest.fixture(scope="session", autouse=True)(
 base_tenant = pytest.fixture(scope="session", autouse=True)(fixtures.base_tenant)
 
 # -- Test-scoped fixtures --
+client = pytest.fixture(scope="function")(fixtures.client)
+auth_client = pytest.fixture(scope="function")(fixtures.auth_client)
 run_id = pytest.fixture(scope="function")(fixtures.run_id)
 test_accounts = pytest.fixture(scope="function")(fixtures.test_accounts)
 test_transactions = pytest.fixture(scope="function")(fixtures.test_transactions)
