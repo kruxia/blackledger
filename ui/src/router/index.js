28e9571189bc96f3a9b21fa03d0e482df3e32cb4
Sean Harrison <sah@kruxia.com>
2024-08-31 16:49:19 -0500

    ui - /ledger has LedgerView template to fill

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -9,14 +9,14 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
+    {
+      path: '/ledger',
+      name: 'ledger',
+      // route level code-splitting
+      // this generates a separate chunk (Ledger.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/LedgerView.vue')
+    }
   ]
 })
 
